407c24de4a1360c2698ef3151d7c2a1a69052ffa
AlexDotRogach <alex.rogach131@ukr.net>
2023-04-13 17:01:41 +0300

    5.2.1

--- index.js
+++ index.js
@@ -1,72 +1,148 @@
-// Реалізуйте клас Worker (Працівник), який матиме
-// такі властивості:
-// name (ім'я), surname (прізвище), rate (ставка за день роботи), days 
-// (кількість відпрацьованих днів).
-// Також клас повинен мати метод getSalary(),
-// який виводитиме зарплату працівника.
-// Зарплата - це добуток (множення) ставки rate на
-// кількість відпрацьованих днів days
+// Визначити клас Reader, який зберігає таку інформацію про користувача бібліотеки:
 
-// зробити можливість додавання відділу, але перевіряти чи є в нього відділ 
-// якщо є то повертаємо помилку.
-
-const workers = [
-  { name: "Erick", surname: "Vasin", rate: 50, days: 21 }, 
-  { name: "Dima", surname: "Test", rate: 20, days: 11 },
-  { name: "Vasya", surname: "Test1", rate: 32, days: 5 }
-];
+// ПІБ,
+// номер читацького квитка,
+// факультет,
+// Методи takeBook(), returnBook().
+// Розробити програму, у якій створюється масив об'єктів цього класу.
+// Перевантажити методи takeBook(), returnBook():
+// - takeBook, який прийматиме кількість взятих книг. Виводить на консоль повідомлення "Alex  взяв 3 книги".
+// - takeBook, який прийматиме змінну кількість назв книг. Виводить на консоль повідомлення "Alex. взяв книги: Пригоди, Словник, Енциклопедія".
+// - takeBook, який прийматиме змінну кількість об'єктів класу Book (створити новий клас, що містить ім'я та автора книги).
+//  Виводить на консоль повідомлення "Alex взяв книги: Пригоди, Словник, Енциклопедія".
+// Аналогічно перевантажити метод returnBook(). Виводить на консоль повідомлення 
+// "Alex повернув книги: Пригоди, Словник, Енциклопедія". Або "Alex повернув 3 книги".
 
 
+const faculties = ["gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"];
 
-class Worker {
-  #department = [];
+const books = [
+  {
+    "author": "Эрнест Хемингуэй",
+    "name": "Старик и море",
+    "amount" : 3
+  },
+  {
+    "author": "Федор Достоевский",
+    "name": "Преступление и наказание",
+    "amount" : 5
+  },
+  {
+    "author": "Лев Толстой",
+    "name": "Война и мир",
+    "amount" : 6
+  },
+  {
+    "author": "Джордж Оруэлл",
+    "name": "1984",
+    "amount" : 2
+  },
+  {
+    "author": "Джейн Остин",
+    "name": "Гордость и предубеждение",
+    "amount" : 3
+  }
+]
 
-  constructor({name, surname, rate, days}) {
-    this.name = name;
-    this.surname = surname;
-    this.rate = rate;
-    this.days = days;
+
+const readers = [
+  {
+    name : "Alex",
+    surname : "Test",
+    number_ticket: 11111,
+    faculty: "gryffindor"
+  },
+  {
+    name : "Dima",
+    surname : "Test1",
+    number_ticket: 11112,
+    faculty: "Slytherin"
+  },
+  {
+    name : "Vasya",
+    surname : "Test2",
+    number_ticket: 11113,
+    faculty: "Slytherin"
+  },
+  {
+    name : "Sergie",
+    surname : "Test3",
+    number_ticket: 11114,
+    faculty: "Ravenclaw"
+  },
+  {
+    name : "Slava",
+    surname : "Test4",
+    number_ticket: 11115,
+    faculty: "Hufflepuff"
+  },
+]
+
+class Reader {
+  static Snotification = {
+    error : {
+      errorByTicketId : "Id isn`t correct",
+      errorByBook : "Some trobules with books"
+    },
+    success : {
+      successTakeBook : (name, amount, nameBook) => `${name} взяв ${amount} книги, назва - ${nameBook}`
+    }
   }
 
-  getSalary() {
-    return this.rate * this.days;
+  #readers;
+  #books;
+  #statics = {};
+
+  constructor (readers, books) {
+    this.#readers = readers;
+    this.#books = books;
   }
 
-  get department () {
-    return [...this.#department];
-  }
+  takeBook (idTicket, amount, nameBook) {
+    const isReaderId = this.#readers.some(reader => reader.number_ticket === idTicket)
 
-  set department (newDepart) {
-
-    if (this.#department.includes(newDepart)) {
-      console.log ('Warning! Such department exists')
+    if (!(isReaderId)) {
+      console.log(Reader.Snotification.error.errorByTicketId);
       return;
     }
 
-    this.#department.push(newDepart)
+    const isBook = this.#books.some(book => book.name === nameBook && book.amount >= amount)
+
+    if (!(isBook)) {
+      console.log(Reader.Snotification.error.errorByBook);
+      return;
+    }
+
+    const reader = this.#readers.find(reader => reader.number_ticket === idTicket);
+
+    console.log(Reader.Snotification.success.successTakeBook(reader.name, amount, nameBook));
+
+    const ourBook = this.#books.find(book => book.name === nameBook);
+
+    if (this.#statics[reader.name]) {
+      this.#statics[reader.name] = [...this.#statics[reader.name], {...ourBook, amount : amount}]
+    } else {
+      this.#statics[reader.name] = [{...ourBook, amount : amount}];
+    }
+
+    ourBook.amount -= amount;
   }
 }
 
+// 1. Перевірка чи є такий айді тікета 
+// 2. Чи є така книга
+// 3. Чи є така кількість книг яка необхідна
 
-const instansesWorkers = workers.map(worker => new Worker(worker));
+// 4. Якщо все гуд - то виводемо консоль лог і додаємо змінні в наші дані (statics, books)
 
-// console.log(instansesWorkers[0].department);
+const mainReader = new Reader(readers, books)
 
-instansesWorkers[0].department.push("test")
-instansesWorkers[0].department.push("test")
-instansesWorkers[0].department.push("test")
+mainReader.takeBook(11112, 2, "Война и мир")
+mainReader.takeBook(11112, 2, "Война и мир")
+mainReader.takeBook(11112, 2, "Война и мир")
+mainReader.takeBook(11112, 2, "Война и мир")
 
 
-// console.log(instansesWorkers[0].department);
 
-// instansesWorkers[0].department.push("test")
-
-// console.log(instansesWorkers[0].department);
-
-instansesWorkers[0].department = "test";
-instansesWorkers[0].department = "test";
-instansesWorkers[0].department = "tesasdsat";
-
-console.log(instansesWorkers[0].department );
 
 
